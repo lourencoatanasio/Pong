464ec8d3f833bd373f2160199188272ad642dbc4
lourencoatanasio <lourencodiogo1234@gmail.com>
2024-10-31 00:22:54 +0000

    not muhc left

--- srcs/megaPong3d.js
+++ srcs/megaPong3d.js
@@ -1,70 +1,72 @@
 import * as THREE from '../three.js-master/build/three.module.js';
 import { OrbitControls } from '../three.js-master/examples/jsm/controls/OrbitControls.js';
 
-function makeParalellepiped(x, y, z, dx, dy, dz, color) 
-{
-  console.log(`Creating box with color: ${color}`); // Debugging line
-  const material = new THREE.MeshStandardMaterial({ color: color });
-  const box = new THREE.Mesh(new THREE.BoxGeometry(dx, dy, dz), material); 
-  box.position.set(x + dx / 2, y + dy / 2, z + dz / 2);
-  return box;
-}
+// Constants
+const PADDLE_SPEED = 10;
+const CUBE_INITIAL_SPEED = 20;
+const SHAKE_INTENSITY = 15;
+const SHAKE_DURATION = 20;
+const PADDLE_COLOR = 0x008000;
+const TABLE_COLOR = 0x800080;
+const PLANE_COLOR = 0x000000;
+const CUBE_COLOR = 0x00ff00;
+const POINT_LIGHT_INTENSITY = 1000000;
+const POINT_LIGHT_DISTANCE = 1000;
+const AMBIENT_LIGHT_INTENSITY = 1;
 
+// Variables
+let player1Score = 0;
+let player2Score = 0;
+let cubeSpeedx = CUBE_INITIAL_SPEED;
+let cubeSpeedz = 0;
+let shakeDuration = 0;
+let paddle1Speed = 0;
+let paddle2Speed = 0;
+
+// Scene Setup
 const canvas = document.getElementById('canvas');
-
 const renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true });
-
 renderer.setSize(window.innerWidth * 0.8, window.innerHeight * 0.8);
-
 document.body.appendChild(renderer.domElement);
 
 const scene = new THREE.Scene();
-
 const camera = new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight, 0.1, 10000);
-
 const controls = new OrbitControls(camera, renderer.domElement);
-
-let player1Score = 0;
-let player2Score = 0;
-
-
-const gridHelper = new THREE.GridHelper(5000, 100, 0x808080);
-scene.add(gridHelper);
-
 camera.position.set(0, 5000, 0);
 controls.update();
 
+// Helpers
+const gridHelper = new THREE.GridHelper(5000, 100, 0x808080);
+scene.add(gridHelper);
+
+// Plane
 const planeGeometry = new THREE.PlaneGeometry(3000, 2000);
-const planeMaterial = new THREE.MeshStandardMaterial({ color: 0x000000 }); // grey 0x808080
+const planeMaterial = new THREE.MeshStandardMaterial({ color: PLANE_COLOR });
 const plane = new THREE.Mesh(planeGeometry, planeMaterial);
 plane.rotation.x = -Math.PI / 2;
-plane.position.y = -100
-// make the plane receive less light
+plane.position.y = -100;
 plane.receiveShadow = true;
 scene.add(plane);
 
-//
-// make ambient light
-//
+// Lights
+const ambientLight = new THREE.AmbientLight(0xffffff, AMBIENT_LIGHT_INTENSITY);
+scene.add(ambientLight);
 
-// white light 0xffffff
-
-
-const geometry = new THREE.BoxGeometry(10, 10, 10);	
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 }); // green 0x00ff00
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
-
-const cubeBoundingBox = new THREE.Box3().setFromObject(cube);
-
-const pointLight = new THREE.PointLight(0xffffff, 100000, 1000); // White light with intensity 1 and distance 100
+const pointLight = new THREE.PointLight(0xffffff, POINT_LIGHT_INTENSITY, POINT_LIGHT_DISTANCE);
 scene.add(pointLight);
 
-// make the pong table using makeParalellepiped
-const table1 = makeParalellepiped(-1300, 0, 500, 2500, 100, 100, 0x800080); // purple 0x800080
-const table2 = makeParalellepiped(-1300, 0, -600, 2500, 100, 100, 0x800080); // purple 0x800080
-const paddle1 = makeParalellepiped(-800, 0, 0, 2, 10, 150, 0x008000); // green 0x008000
-const paddle2 = makeParalellepiped(800, 0, 0, 2, 10, 150, 0x008000); // green 0x008000
+// Cube
+const cubeGeometry = new THREE.BoxGeometry(10, 10, 10);
+const cubeMaterial = new THREE.MeshBasicMaterial({ color: CUBE_COLOR });
+const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+scene.add(cube);
+const cubeBoundingBox = new THREE.Box3().setFromObject(cube);
+
+// Paddles and Table
+const table1 = makeParalellepiped(-1300, 0, 500, 2500, 100, 100, TABLE_COLOR);
+const table2 = makeParalellepiped(-1300, 0, -600, 2500, 100, 100, TABLE_COLOR);
+const paddle1 = makeParalellepiped(-800, 0, 0, 4, 10, 200, PADDLE_COLOR);
+const paddle2 = makeParalellepiped(800, 0, 0, 4, 10, 200, PADDLE_COLOR);
 
 scene.add(table1);
 scene.add(table2);
@@ -76,177 +78,150 @@ const table2BoundingBox = new THREE.Box3().setFromObject(table2);
 const paddle1BoundingBox = new THREE.Box3().setFromObject(paddle1);
 const paddle2BoundingBox = new THREE.Box3().setFromObject(paddle2);
 
-const ambientLight = new THREE.AmbientLight(0xffffff, 1);
-scene.add(ambientLight);
+// Functions
+function makeParalellepiped(x, y, z, dx, dy, dz, color) 
+{
+  const material = new THREE.MeshStandardMaterial({ color: color });
+  const box = new THREE.Mesh(new THREE.BoxGeometry(dx, dy, dz), material);
+  box.position.set(x + dx / 2, y + dy / 2, z + dz / 2);
+  return box;
+}
 
-// Variables to track cube movement
-let cubeSpeedx = 5;
-let cubeSpeedz = 5;
-let shakeDuration = 0;
-const shakeIntensity = 15;
+function handlePaddleControls() 
+{
+  document.addEventListener('keydown', (event) => 
+  {
+    switch (event.key) 
+    {
+      case 'w':
+        paddle1Speed = -PADDLE_SPEED;
+        break;
+      case 's':
+        paddle1Speed = PADDLE_SPEED;
+        break;
+      case 'ArrowUp':
+        paddle2Speed = -PADDLE_SPEED;
+        break;
+      case 'ArrowDown':
+        paddle2Speed = PADDLE_SPEED;
+        break;
+    }
+  });
 
-// Variables to track paddle movement
-let paddle1Speed = 0;
-let paddle2Speed = 0;
-const paddleSpeed = 10; // Speed of the paddles
+  document.addEventListener('keyup', (event) => 
+  {
+    switch (event.key) 
+    {
+      case 'w':
+      case 's':
+        paddle1Speed = 0;
+        break;
+      case 'ArrowUp':
+      case 'ArrowDown':
+        paddle2Speed = 0;
+        break;
+    }
+  });
+}
 
-// Event listeners for keydown and keyup
-document.addEventListener('keydown', (event) => {
-  switch (event.key) {
-    case 'w': // Move paddle1 up
-      paddle1Speed = -paddleSpeed;
-      break;
-    case 's': // Move paddle1 down
-      paddle1Speed = paddleSpeed;
-      break;
-    case 'ArrowUp': // Move paddle2 up
-      paddle2Speed = -paddleSpeed;
-      break;
-    case 'ArrowDown': // Move paddle2 down
-      paddle2Speed = paddleSpeed;
-      break;
-  }
-});
-
-document.addEventListener('keyup', (event) => {
-  switch (event.key) {
-    case 'w':
-    case 's':
-      paddle1Speed = 0;
-      break;
-    case 'ArrowUp':
-    case 'ArrowDown':
-      paddle2Speed = 0;
-      break;
-  }
-});
-
-function movePaddles() {
-  // Move paddles
+function movePaddles() 
+{
   paddle1.position.z += paddle1Speed;
   paddle2.position.z += paddle2Speed;
-
-  // Update paddles' bounding boxes
   paddle1BoundingBox.setFromObject(paddle1);
   paddle2BoundingBox.setFromObject(paddle2);
 }
 
-function applyCameraShake() {
-  if (shakeDuration > 0) {
-    const shakeX = (Math.random() - 0.5) * shakeIntensity;
-    const shakeY = (Math.random() - 0.5) * shakeIntensity;
-    const shakeZ = (Math.random() - 0.5) * shakeIntensity;
+function applyCameraShake() 
+{
+  if (shakeDuration > 0) 
+  {
+    const shakeX = (Math.random() - 0.5) * SHAKE_INTENSITY;
+    const shakeY = (Math.random() - 0.5) * SHAKE_INTENSITY;
+    const shakeZ = (Math.random() - 0.5) * SHAKE_INTENSITY;
     camera.position.x += shakeX;
     camera.position.y += shakeY;
     camera.position.z += shakeZ;
     shakeDuration--;
   }
-  if (shakeDuration == 0) {
+  if (shakeDuration == 0)   
+  {
     camera.position.set(0, 500, 0);
   }
 }
 
 function increaseSpeed() 
 {
-  if (cubeSpeedx > 0) 
-  {
-    cubeSpeedx += 0.2;
-  }
-  else 
-  {
-    cubeSpeedx -= 0.2;
-  }
-  if (cubeSpeedz > 0) 
-  {
-    cubeSpeedz += 0.2;
-  }
-  else 
-  {
-    cubeSpeedz -= 0.2;
-  }
+  if (cubeSpeedx < 20 && cubeSpeedx > -20)
+    cubeSpeedx += (cubeSpeedx > 0) ? 0.2 : -0.2;
 }
 
-function checkIntersections() {
+function checkIntersections() 
+{
   cubeBoundingBox.setFromObject(cube);
 
-  if (cubeBoundingBox.intersectsBox(table1BoundingBox) || cubeBoundingBox.intersectsBox(table2BoundingBox)) {
+  if (cubeBoundingBox.intersectsBox(table1BoundingBox) || cubeBoundingBox.intersectsBox(table2BoundingBox)) 
+  {
     cubeSpeedz *= -1;
-    console.log(cubeSpeedz);
-    shakeDuration = 20;
-    increaseSpeed();
-  }
-  if (cubeBoundingBox.intersectsBox(paddle1BoundingBox) || cubeBoundingBox.intersectsBox(paddle2BoundingBox)) {
-    cubeSpeedx *= -1;
-    console.log(cubeSpeedx);
-    shakeDuration = 20;
-    increaseSpeed();
+    shakeDuration = SHAKE_DURATION;
   }
 
-  // add paddle collision with table
-  if (paddle1BoundingBox.intersectsBox(table1BoundingBox) || paddle1BoundingBox.intersectsBox(table2BoundingBox)) {
+  if (cubeBoundingBox.intersectsBox(paddle1BoundingBox)) 
+  {
+    cubeSpeedx *= -1;
+    shakeDuration = SHAKE_DURATION;
+    increaseSpeed();
+    adjustCubeDirection(paddle1);
+    console.log(cubeSpeedx);
+  }
+
+  if (cubeBoundingBox.intersectsBox(paddle2BoundingBox)) 
+  {
+    cubeSpeedx *= -1;
+    shakeDuration = SHAKE_DURATION;
+    increaseSpeed();
+    adjustCubeDirection(paddle2);
+    console.log(cubeSpeedx);
+  }
+
+  if (paddle1BoundingBox.intersectsBox(table1BoundingBox) || paddle1BoundingBox.intersectsBox(table2BoundingBox)) 
+  {
     paddle1.position.z -= paddle1Speed;
   }
-  if (paddle2BoundingBox.intersectsBox(table1BoundingBox) || paddle2BoundingBox.intersectsBox(table2BoundingBox)) {
+  if (paddle2BoundingBox.intersectsBox(table1BoundingBox) || paddle2BoundingBox.intersectsBox(table2BoundingBox)) 
+  {
     paddle2.position.z -= paddle2Speed;
   }
 }
 
-function randomDirection() // figure out how to make the cube go in a random direction
+function adjustCubeDirection(paddle) 
 {
-  let fullSpeed = 10;
-  let random = Math.random();
-  if(random < 0.5)
-  {
-    cubeSpeedx = fullSpeed * Math.random();
-    if (cubeSpeedx == 0)
-    {
-      cubeSpeedx = 1;
-    }
-    cubeSpeedz = fullSpeed - cubeSpeedx;
-  }
-  else
-  {
-    cubeSpeedz = fullSpeed * Math.random();
-    cubeSpeedx = fullSpeed - cubeSpeedz;
-  }
-  console.log('Full speed');
-  console.log(fullSpeed);
-  console.log('Cube speed x');
-  console.log(cubeSpeedx);
-  console.log('Cube speed z');
-  console.log(cubeSpeedz);
+  const relativeIntersectZ = (paddle.position.z + (paddle.geometry.parameters.depth / 2)) - cube.position.z;
+  const normalizedIntersectZ = (relativeIntersectZ / (paddle.geometry.parameters.depth / 2)) - 1;
+  cubeSpeedz = normalizedIntersectZ * 5; // Adjust the multiplier as needed
 }
 
-function respawnCube(point) 
+function respawnCube() 
 {
-  if(point == 1)
-  {
-    cube.position.set(0, 0, 0);
-    randomDirection();
-  }
-  else
-  {
-    cube.position.set(0, 0, 0);
-    randomDirection();
-  }
+  cube.position.set(0, 0, 0);
+  cubeSpeedx = CUBE_INITIAL_SPEED;
+  cubeSpeedz = 0;
 }
 
-function moveCube() 
+function moveCube()
 {
   cube.rotation.x += 0.01;
   cube.rotation.y += 0.01;
-
   cube.position.x += cubeSpeedx;
   cube.position.z += cubeSpeedz;
   pointLight.position.copy(cube.position);
 
-  if (cube.position.x > 1000)
+  if (cube.position.x > 1000) 
   {
     respawnCube();
     player1Score++;
     document.getElementById('player1score').innerHTML = player1Score;
-  }
-  else if(cube.position.x < -1000) 
+  } else if (cube.position.x < -1000) 
   {
     respawnCube();
     player2Score++;
@@ -258,29 +233,47 @@ function moveCube()
 
 function paddle1AI(paddle, cube) 
 {
-  if(cube.position.x < 0)
+  if (cube.position.x < 0) 
   {
-    if (paddle.position.z > cube.position.z)
+    if (paddle.position.z > cube.position.z && paddle.position.z > -500) 
     {
-      paddle.position.z -= paddleSpeed;
-    } 
-    else if (paddle.position.z < cube.position.z) 
+      // check if the paddle is not out of bounds
+      if (paddle.position.z + paddle.geometry.parameters.depth / 2 > 500)
+      {
+        paddle.position.z = 500 - paddle.geometry.parameters.depth / 2;
+      }
+      else 
+      {
+        paddle.position.z -= PADDLE_SPEED;
+      }
+    }
+    else if (paddle.position.z < cube.position.z && paddle.position.z < 500) 
     {
-      paddle.position.z += paddleSpeed;
-    } 
+      // check if the paddle is not out of bounds
+      if (paddle.position.z - paddle.geometry.parameters.depth / 2 < -500)
+      {
+        paddle.position.z = -500 + paddle.geometry.parameters.depth / 2;
+      }
+      else 
+      {
+        paddle.position.z += PADDLE_SPEED;
+      }
+    }
   }
 }
 
 function animate() 
 {
-  if (player1Score < 7 && player2Score < 7) {
-    moveCube();
+  if (player1Score < 7 && player2Score < 7) 
+  {
     movePaddles();
-    paddle1AI(paddle1, cube);
     checkIntersections();
+    moveCube();
+    paddle1AI(paddle1, cube);
     applyCameraShake();
     renderer.render(scene, camera);
   }
 }
 
+handlePaddleControls();
 renderer.setAnimationLoop(animate);
